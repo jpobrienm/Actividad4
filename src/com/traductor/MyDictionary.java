551140bbc9f944e8de7506a5aec9ef4e4d96d357
jpobrienm <33789137+jpobrienm@users.noreply.github.com>
2021-11-11 16:04:49 -0500

    se convierte la clase MyDict en una interfaz por escalabilidad, se crea una nueva clase Spanish_English que implementará la interfaz

--- src/com/traductor/MyDictionary.java
+++ src/com/traductor/MyDictionary.java
@@ -1,4 +1,8 @@
 package com.traductor;
 
-public class MyDictionary {
+interface MyDictionary {
+    public void readDict(String file);
+    public void editDict(String file, String key, String definition);
+    public void addToDict(String file, String key, String definition);
+    public void removeFromDict(String file, String key);
 }
---
+++ src/com/traductor/Spanish_English.java
@@ -0,0 +1,23 @@
+package com.traductor;
+
+public class Spanish_English implements MyDictionary{
+    @Override
+    public void readDict(String file) {
+        
+    }
+
+    @Override
+    public void editDict(String file, String key, String definition) {
+
+    }
+
+    @Override
+    public void addToDict(String file, String key, String definition) {
+
+    }
+
+    @Override
+    public void removeFromDict(String file, String key) {
+
+    }
+}
